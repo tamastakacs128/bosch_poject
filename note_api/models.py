fe3f6d5c2878a65ed0261851c937009cd6d92857
fabletomi <tamas.takacs128@gmail.com>
2023-03-01 04:53:29 +0100

    set up models, serializers, urls, views

--- note_api/models.py
+++ note_api/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class NoteModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    year_of_birth = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
